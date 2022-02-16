ddf108a81b0a7672fc7369a9b2bba2d558f9089b
afmansano <afmansano@gmail.com>
2022-02-16 17:55:23 -0300

    bug correction, added python=3.8 as dependency to download

--- main.py
+++ main.py
@@ -20,8 +20,8 @@ def go(config: DictConfig):
         # This was passed on the command line as a comma-separated list of steps
         steps_to_execute = config["main"]["execute_steps"].split(",")
     else:
-        assert isinstance(config["main"]["execute_steps"], list)
-        steps_to_execute = config["main"]["execute_steps"]
+
+        steps_to_execute = list(config["main"]["execute_steps"])
 
     # Download step
     if "download" in steps_to_execute:
@@ -38,7 +38,6 @@ def go(config: DictConfig):
         )
 
     if "preprocess" in steps_to_execute:
-
         _ = mlflow.run(
             os.path.join(root_path, "preprocess"),
             "main",
@@ -46,12 +45,11 @@ def go(config: DictConfig):
                 "input_artifact": "raw_data.parquet:latest",
                 "artifact_name": "preprocessed_data.csv",
                 "artifact_type": "preprocessed_data",
-                "artifact_description": "Data after proprocessed step applied"
-            }
+                "artifact_description": "Data with preprocessing applied"
+            },
         )
 
     if "check_data" in steps_to_execute:
-
         _ = mlflow.run(
             os.path.join(root_path, "check_data"),
             "main",
@@ -59,7 +57,7 @@ def go(config: DictConfig):
                 "reference_artifact": config["data"]["reference_dataset"],
                 "sample_artifact": "preprocessed_data.csv:latest",
                 "ks_alpha": config["data"]["ks_alpha"]
-            }
+            },
         )
 
     if "segregate" in steps_to_execute:
@@ -72,13 +70,11 @@ def go(config: DictConfig):
                 "artifact_root": "data",
                 "artifact_type": "segregated_data",
                 "test_size": config["data"]["test_size"],
-                "random_state": config["data"]["random_state"],
                 "stratify": config["data"]["stratify"]
-            }
+            },
         )
 
     if "random_forest" in steps_to_execute:
-
         # Serialize decision tree configuration
         model_config = os.path.abspath("random_forest_config.yml")
 
@@ -93,9 +89,9 @@ def go(config: DictConfig):
                 "model_config": model_config,
                 "export_artifact": config["random_forest_pipeline"]["export_artifact"],
                 "random_seed": config["main"]["random_seed"],
-                "val_size": config["data"]["val_size"],
+                "val_size": config["data"]["test_size"],
                 "stratify": config["data"]["stratify"]
-            }
+            },
         )
 
     if "evaluate" in steps_to_execute:
@@ -104,9 +100,9 @@ def go(config: DictConfig):
             os.path.join(root_path, "evaluate"),
             "main",
             parameters={
-                "model_export": f"{config["random_forest_pipeline"]["export_artifact"]}:latest",
+                "model_export": f"{config['random_forest_pipeline']['export_artifact']}:latest",
                 "test_data": "data_test.csv:latest"
-            }
+            },
         )
 
 
